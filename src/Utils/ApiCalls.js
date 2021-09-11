3edeaf9bacfbd0fd037bb210f806680bf2a41100
Maria DelSignore <maria.ad27@gmail.com>
2021-09-11 11:54:47 -0600

    Add param to get data

--- src/Utils/ApiCalls.js
+++ src/Utils/ApiCalls.js
@@ -1,5 +1,5 @@
-const getData = () => {
-  fetch('https://api.spacexdata.com/v4/rockets')
+const getData = (category) => {
+  fetch(`https://api.spacexdata.com/v4/${category}`)
     .then(res => res.json())
     .then(data => console.log(data))
 }
