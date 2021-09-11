be57fbab5be5791c5be0e7730f6f684755e9da23
StevenTheBearded <stevenberg22@gmail.com>
2021-09-11 14:30:47 -0400

    Change structure of cards

--- src/components/HistoricalCards/HistoricalCards.js
+++ src/components/HistoricalCards/HistoricalCards.js
@@ -4,10 +4,20 @@ function HistoricalCards() {
 
   return (
     <article>
-      <p>Title of article</p>
-      <p>"Body oddy oddy oddy" - Meg Thee Stallion</p>
+      <p className='article'>Article Screenshot/hpyerlink?</p>
+      <p className='details'>"Body oddy oddy oddy" - Meg Thee Stallion</p>
+      <p className='title'>title</p>
     </article>
   )
 }
 
 export default HistoricalCards;
+
+// "links": {
+//   "article": "http://www.spacex.com/news/2013/02/11/flight-4-launch-update-0"
+//   },
+//   "title": "Falcon reaches Earth orbit",
+//   "event_date_utc": "2008-09-28T23:15:00Z",
+//   "event_date_unix": 1222643700,
+//   "details": "Falcon 1 becomes the first privately developed liquid-fuel rocket to reach Earth orbit.",
+//   "id": "5f6fb2cfdcfdf403df37971e"
