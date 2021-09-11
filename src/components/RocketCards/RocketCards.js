66d0da50e163b137affe54b98427f3d4e7c28af4
Megan McBride <megan.mcbride@colorado.edu>
2021-09-11 16:22:17 -0600

    Added styling to rocket card

--- src/components/RocketCards/RocketCards.js
+++ src/components/RocketCards/RocketCards.js
@@ -3,20 +3,22 @@ import './RocketCards.css';
 
 const RocketCards = ({ id, name, description, cost, firstFlight, image, height, diameter, mass }) => {
   return (
-    <section>
-      <h1>{name}</h1>
-      <section>
-        <img src={image} alt={`rocket ${name}`} />
+    <main className='rocket-card'>
+      <h1 className='rocket-name'>{name}</h1>
+      <section className='card-body'>
+        <section className='rocket-pic-container'>
+          <img className='rocket-pic'src={image} alt={`rocket ${name}`} />
+        </section>
+        <section>
+          <p>{description}</p>
+          <p>First Flight: {firstFlight}</p>
+          <p>Cost Per Flight: {cost}</p>
+          <p>Height: {height} ft.</p>
+          <p>Diameter: {diameter} ft.</p>
+          <p>Mass: {mass} lb.</p>
+        </section>
       </section>
-      <section>
-        <p>{description}</p>
-        <p>First Flight: {firstFlight}</p>
-        <p>Cost Per Flight: {cost}</p>
-        <p>Height: {height} ft.</p>
-        <p>Diameter: {diameter} ft.</p>
-        <p>Mass: {mass} lb.</p>
-      </section>
-    </section>
+    </main>
   )
 }
 
