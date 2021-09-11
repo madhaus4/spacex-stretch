13a4b390e90a72c13fbd726a9f87a6dd9407e9bc
StevenTheBearded <stevenberg22@gmail.com>
2021-09-11 14:01:24 -0400

    create functional component to render cards

---
+++ src/components/HistoricalCards/HistoricalCards.js
@@ -0,0 +1,13 @@
+import './HistoricalCards.css';
+
+function HistoricalCards() {
+
+  return (
+    <article>
+      <p>Title of article</p>
+      <p>"Body oddy oddy oddy" - Meg Thee Stallion</p>
+    </article>
+  )
+}
+
+export default HistoricalCards;
