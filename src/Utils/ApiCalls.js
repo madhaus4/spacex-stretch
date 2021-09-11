1cb330b5a0524bfbb00541430756f28911dd6565
Maria DelSignore <maria.ad27@gmail.com>
2021-09-11 11:56:03 -0600

    Remove console log

--- src/Utils/ApiCalls.js
+++ src/Utils/ApiCalls.js
@@ -1,7 +1,7 @@
 const getData = (category) => {
   fetch(`https://api.spacexdata.com/v4/${category}`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => data)
 }
 
 
