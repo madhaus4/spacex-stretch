cc478cd60cb3dad118e0e158fc84bc1ebd41ddfb
StevenTheBearded <stevenberg22@gmail.com>
2021-09-11 15:03:08 -0400

    Change paths of imports

---
+++ src/components/RocketCards/RocketCards.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './RocketCards.css';
+
+const RocketCards = ({ id, name, description, cost, image, height, diameter, mass }) => {
+  return (
+    <section>
+      <h1>{name}</h1>
+      <section>
+        <img src={image} alt={`rocket ${name}`} />
+      </section>
+      <section>
+        <p>{description}</p>
+        <p>Cost: {cost}</p>
+        <p>Height: {height}</p>
+        <p>Diameter: {diameter}</p>
+        <p>Mass: {mass}</p>
+      </section>
+    </section>
+  )
+}
+
+export default RocketCards;
