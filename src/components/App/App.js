a28f5802146279b3ed84cb4de1c65ee922f99621
Megan McBride <megan.mcbride@colorado.edu>
2021-09-11 14:24:22 -0600

    Troubleshoot container

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -29,6 +29,7 @@ function App() {
       
       <Route path='/' render={() => <Header /> } />
       <HistoricalContainer />
+     <RocketContainer allRockets={rockets} />
     </main>
   );
 }
--- src/components/Rocket-Container/Rocket-Container.js
+++ src/components/Rocket-Container/Rocket-Container.js
@@ -2,24 +2,25 @@ import React from 'react';
 import './RocketContainer.css';
 import RocketCards from '../RocketCards';
 
-const RocketContainer = ({ rockets }) => {
-  const allRockets = rockets.map(rocket => (
-    <RocketCards
-      key={rocket.name}
-      name={rocket.name}
-      description={rocket.description}
-      cost={rocket.cost_per_launch}
-      firstFlight={rocket.first_flight}
-      image={rocket.flickr_images[0]}
-      height={rocket.height.feet}
-      diameter={rocket.diameter.feet}
-      mass={rocket.mass.lb}
-    />
-  ))
+const RocketContainer = (props) => {
+  {console.log(props.allRockets)}
+  // const allRockets = rockets.map(rocket => (
+  //   <RocketCards
+  //     key={rocket.name}
+  //     name={rocket.name}
+  //     description={rocket.description}
+  //     cost={rocket.cost_per_launch}
+  //     firstFlight={rocket.first_flight}
+  //     image={rocket.flickr_images[0]}
+  //     height={rocket.height.feet}
+  //     diameter={rocket.diameter.feet}
+  //     mass={rocket.mass.lb}
+  //   />
+  // ))
 
   return(
     <section className='rocket-container'>     
-      {allRockets}          
+      <h1>Am I here?</h1>      
     </section>
   )
 
