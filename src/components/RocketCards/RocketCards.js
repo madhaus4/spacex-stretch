35083989c27113d873cb56227ae822e4c395d182
Megan McBride <megan.mcbride@colorado.edu>
2021-09-11 21:02:16 -0600

    Moved info into a different section for styling

--- src/components/RocketCards/RocketCards.js
+++ src/components/RocketCards/RocketCards.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './RocketCards.css';
 
-const RocketCards = ({ id, name, description, cost, firstFlight, image, height, diameter, mass }) => {
+const RocketCards = ({ name, description, cost, firstFlight, image, height, diameter, mass }) => {
   return (
     <main className='rocket-card'>
       <h1 className='rocket-name'>{name}</h1>
@@ -9,13 +9,23 @@ const RocketCards = ({ id, name, description, cost, firstFlight, image, height, 
         <section className='rocket-pic-container'>
           <img className='rocket-pic'src={image} alt={`rocket ${name}`} />
         </section>
-        <section rocket='rocket-info'>
-          <p className='details'>{description}</p>
-          <p className='first-flight'>First Flight: {firstFlight}</p>
-          <p className='flight-cost'>Cost Per Flight: {cost}</p>
-          <p className='rocket-height'>Height: {height} ft.</p>
-          <p className='rocket-diameter'>Diameter: {diameter} ft.</p>
-          <p className='rocket-mass'>Mass: {mass} lb.</p>
+        <section className='rocket-info'>
+          <section>
+            <h2>The Story</h2>
+            <h2>First Flight:</h2>
+            <h2>Cost Per Flight:</h2>
+            <h2>Height: </h2>
+            <h2>Diameter: </h2>
+            <h2>Mass: </h2>
+          </section>
+          <section>
+            <p className='details'>{description}</p>
+            <p className='first-flight'>{firstFlight}</p>
+            <p className='flight-cost'>Cost Per Flight: {cost}</p>
+            <p className='rocket-height'>Height: {height} ft.</p>
+            <p className='rocket-diameter'>Diameter: {diameter} ft.</p>
+            <p className='rocket-mass'>Mass: {mass} lb.</p>
+          </section>
         </section>
       </section>
     </main>
