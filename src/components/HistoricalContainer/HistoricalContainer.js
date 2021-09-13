13d5034a168e2e189452d7de7141c0afbe6e4f8e
Megan McBride <megan.mcbride@colorado.edu>
2021-09-13 10:20:49 -0600

    Add the article details to the container page

--- src/components/HistoricalContainer/HistoricalContainer.js
+++ src/components/HistoricalContainer/HistoricalContainer.js
@@ -1,11 +1,22 @@
 import './HistoricalContainer.css';
 import HistoricalCards from '../HistoricalCards/HistoricalCards';
 
-function HistoricalContainer() {
-
+function HistoricalContainer({ theHistory }) {
+  const allHistory = theHistory.filter(story => {
+    return story.links.article && !story.links.article.includes('www.spacex.com')      
+  }).map(story => (
+    <HistoricalCards 
+      key={story.id}
+      title={story.title}
+      eventDate={story.event_date_utc}
+      details={story.details}
+      link={story.links.article}  
+    /> 
+  ))
+  
   return(
     <section className='historical-cards-section'>
-      <HistoricalCards /> 
+      
     </section>
   )
 }
