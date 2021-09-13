871f20d8c7bf3713664b30e1d6655761ffb308b6
Maria DelSignore <maria.ad27@gmail.com>
2021-09-13 17:27:33 -0600

    Add add to favroites functionality

--- src/components/HistoricalContainer/HistoricalContainer.js
+++ src/components/HistoricalContainer/HistoricalContainer.js
@@ -3,30 +3,41 @@ import './HistoricalContainer.css';
 import HistoricalCards from '../HistoricalCards/HistoricalCards';
 
 function HistoricalContainer({ theHistory, launchImages }) {
-  const [isFavorited, setIsFavorited] = useState(true)
+  const [isFavorited, setIsFavorited] = useState(false)
+  const [favoritesList, setFavoritesList] = useState([])
 
-  // function will set state 
+  const addToFavorites = (story) => {
+    let newFavorite = {
+      id: Date.now(), ...story
+    }
+    setFavoritesList([...favoritesList, newFavorite])
+  }
+
 
   const allHistory = theHistory.filter(story => {
     return story.links.article && !story.links.article.includes('www.spacex.com')      
   }).map((story, i) => (
+    // console.log('story2', story)
     <HistoricalCards 
       key={story.id}
-      id={story.id}
-      title={story.title}
-      details={story.details}
-      link={story.links.article} 
+      story={story}
+      // id={story.id}
+      // title={story.title}
+      // details={story.details}
+      // link={story.links.article} 
       image={launchImages[i]} 
-      // function={function}
+      addToFavorites={addToFavorites}
+      isFavorited={isFavorited}
     /> 
-  ))
-  
-  return(
-    <section className='historical-cards-section'>
-      <button>View Favorites</button>
-      <button>View All</button>
-      {allHistory}
-    </section>
+    ))
+    
+    return(
+      <section className='historical-cards-section'>
+        {console.log('favoritesList', favoritesList)}
+        <button>View Favorites</button>
+        <button>View All</button>
+        {allHistory}
+      </section>
   )
 }
 
