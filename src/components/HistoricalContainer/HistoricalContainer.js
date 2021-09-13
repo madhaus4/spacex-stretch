688b17c742d7b7a6cd7f900b953dfeb47f237111
Maria DelSignore <maria.ad27@gmail.com>
2021-09-13 14:49:09 -0600

    Add boolean state to property

--- src/components/HistoricalContainer/HistoricalContainer.js
+++ src/components/HistoricalContainer/HistoricalContainer.js
@@ -1,7 +1,12 @@
+import React, { useState } from 'react';
 import './HistoricalContainer.css';
 import HistoricalCards from '../HistoricalCards/HistoricalCards';
 
 function HistoricalContainer({ theHistory, launchImages }) {
+  const [isFavorited, setIsFavorited] = useState(true)
+
+
+
   const allHistory = theHistory.filter(story => {
     return story.links.article && !story.links.article.includes('www.spacex.com')      
   }).map((story, i) => (
@@ -16,6 +21,8 @@ function HistoricalContainer({ theHistory, launchImages }) {
   
   return(
     <section className='historical-cards-section'>
+      <button>View Favorites</button>
+      <button>View All</button>
       {allHistory}
     </section>
   )
