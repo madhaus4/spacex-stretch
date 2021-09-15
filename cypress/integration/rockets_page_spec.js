162e554334059270378aba441737754e54d5957d
Megan McBride <megan.mcbride@colorado.edu>
2021-09-15 11:33:06 -0600

    Test the return of all rocket card information for the first card

--- cypress/integration/rockets_page_spec.js
+++ cypress/integration/rockets_page_spec.js
@@ -21,4 +21,14 @@ describe('Rockets Page View', () => {
     cy.get('.rocket-card').first().get('img').should('have.attr', 'src').should('include', 'https://imgur.com/azYafd8.jpg')
       .get('img[src="https://farm4.staticflickr.com/3955/32915197674_eee74d81bb_b.jpg"]').should('be.visible')
   })
+
+  it('Should display all the information for a rocket card', () => {
+    cy.get('main').first().get('p').eq(0).should('contain', 'The Falcon 1 was an expendable launch system privately')
+    .get('main').first().get('p').eq(1).should('contain', '03/24/2006')
+    .get('main').first().get('p').eq(2).should('contain', '6,700,000')
+    .get('main').first().get('p').eq(3).should('contain', '73')
+    .get('main').first().get('p').eq(4).should('contain', '5.5')
+    .get('main').first().get('p').eq(5).should('contain', '66,460')    
+  })
+
 })
