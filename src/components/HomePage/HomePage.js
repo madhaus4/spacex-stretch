07cc0ca24472dfe92c31d85a43e573544ffb80c5
Maria DelSignore <maria.ad27@gmail.com>
2021-09-15 10:02:21 -0600

    Add text to page header

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,7 +3,7 @@ import './HomePage.css';
 function HomePage() {
   return (
     <section className='title-container'>
-      <h1 className='page-header'>TEXT Here</h1>
+      <h1 className='page-header'>An exploration of SpaceX...</h1>
       <div className='animated-rocket'>IMG Here</div>
     </section>  
   )
