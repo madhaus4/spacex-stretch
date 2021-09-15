895bf8e002ffbbe76244624013fa2bc85ac35693
Maria DelSignore <maria.ad27@gmail.com>
2021-09-15 10:56:34 -0600

    Add div around image to fit content

--- src/components/HistoricalCards/HistoricalCards.js
+++ src/components/HistoricalCards/HistoricalCards.js
@@ -3,7 +3,9 @@ import './HistoricalCards.css';
 function HistoricalCards({ story, image, updateFavorites }) {
   return (
     <article className='historical-card' id={story.id}>
-      <img className='rocket-image' src={image} />
+      <div className='img-wrapper'>
+        <img className='rocket-image' src={image} />
+      </div>
       <div className='info-wrapper'>
         <h3>{story.title} ....click here to view all</h3>
         <h3 className='article-details'>{story.details}</h3>
