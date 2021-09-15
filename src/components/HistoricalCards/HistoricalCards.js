b347857d9507432116f4e113d6300865d3298524
StevenTheBearded <stevenberg22@gmail.com>
2021-09-14 21:44:36 -0400

    Remove tinylink npm package

--- src/components/HistoricalCards/HistoricalCards.js
+++ src/components/HistoricalCards/HistoricalCards.js
@@ -4,20 +4,28 @@ import './HistoricalCards.css';
 function HistoricalCards({ story, image, updateFavorites }) {
   return (
     <article className='historical-card' id={story.id}>
-      <ReactTinyLink
-        cardSize="large"
-        showGraphic={true}
-        maxLine={2}
-        minLine={1}
-        url={story.links.article}
-        header={story.title}
-        description={story.details}
-        defaultMedia={image}
-      />
-      <button
-        onClick={() => updateFavorites(story)}
-        >✨
-      </button>
+      {/* <div className='div-wrap'>
+        <ReactTinyLink
+          cardSize="large"
+          showGraphic={true}
+          maxLine={2}
+          minLine={1}
+          url={story.links.article}
+          header={story.title}
+          description={story.details}
+          defaultMedia={image}
+        />
+        </div> */}
+        <div className='linked-article'>
+          <img className='rocket-image' src={image} />
+          <h2>{story.title} ....click here to view all</h2>
+          <h2>{story.details}</h2>
+        </div>
+        <button className='favorite-btn'
+          onClick={() => updateFavorites(story)}
+          >✨
+        </button>
+      
     </article>
   )
 }
