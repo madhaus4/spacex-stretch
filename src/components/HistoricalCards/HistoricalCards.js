77aa25499317adc3826882655bf11f64192cf6cd
Maria DelSignore <maria.ad27@gmail.com>
2021-09-15 10:53:13 -0600

    Add class names to elements in article

--- src/components/HistoricalCards/HistoricalCards.js
+++ src/components/HistoricalCards/HistoricalCards.js
@@ -1,31 +1,19 @@
-import { ReactTinyLink } from 'react-tiny-link';
 import './HistoricalCards.css';
 
 function HistoricalCards({ story, image, updateFavorites }) {
   return (
     <article className='historical-card' id={story.id}>
-      {/* <div className='div-wrap'>
-        <ReactTinyLink
-          cardSize="large"
-          showGraphic={true}
-          maxLine={2}
-          minLine={1}
-          url={story.links.article}
-          header={story.title}
-          description={story.details}
-          defaultMedia={image}
-        />
-        </div> */}
-        <div className='linked-article'>
-          <img className='rocket-image' src={image} />
-          <h2>{story.title} ....click here to view all</h2>
-          <h2>{story.details}</h2>
-        </div>
+      <div className='img-wrapper'>
+        <img className='rocket-image' src={image} />
+      </div>
+      <div className='info-wrapper'>
+        <h3>{story.title} ....click here to view all</h3>
+        <h3 className='article-details'>{story.details}</h3>
         <button className='favorite-btn'
           onClick={() => updateFavorites(story)}
           >✨
         </button>
-      
+      </div>
     </article>
   )
 }
