1dc960d747ac4b540fdb1b380b85b870e8b70e09
Maria DelSignore <maria.ad27@gmail.com>
2021-09-15 09:46:35 -0600

    Create Home Page functional component

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <section>
+      <h1 className='page-header'>TEXT Here</h1>
+      <div className='animated-rocket'>IMG Here</div>
+    </section>  
+  )
+}
+
+export default HomePage;
