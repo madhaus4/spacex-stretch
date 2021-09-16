24e5000bff23bae42df32ade6f9d8ccfb4807b59
Maria DelSignore <maria.ad27@gmail.com>
2021-09-15 23:12:37 -0600

    Add classnames to elements

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,9 +2,15 @@ import './HomePage.css';
 
 function HomePage() {
   return (
-    <section className='title-container'>
-      <h1 className='page-header'>An exploration of SpaceX...</h1>
-      <div className='animated-rocket'>IMG Here</div>
+    <section className='page-title-container'>
+      <div className='page-details'>
+        <h1 className='page-header'>An exploration of SpaceX...</h1>
+        <p>The family of Falcon launch vehicles are the first and only orbital class rockets that are resuseable!  They are able to successfully land back on earth AND lower the cost of space access by being able to refly.   </p>
+      </div>
+      <div
+        className='animated-rocket-here'
+        >IMG Here
+      </div>
     </section>  
   )
 }
