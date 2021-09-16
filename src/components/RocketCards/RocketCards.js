e0012c4a24448b6b57334a5de4545c2fd065cbc0
Maria DelSignore <maria.ad27@gmail.com>
2021-09-15 21:49:16 -0600

    Remove spacing

--- src/components/RocketCards/RocketCards.js
+++ src/components/RocketCards/RocketCards.js
@@ -6,22 +6,21 @@ const RocketCards = ({ name, description, cost, firstFlight, image, height, diam
     <article className='rocket-card'>
       <h1 className='rocket-name'>{name}</h1>
       <section className='card-body'>
-
         <div className='rocket-image-container'>
           <img className='rocket-image'src={image} alt={`rocket ${name}`} />
         </div>
-
         <section className='rocket-info'>
           <div className='description-wrapper'>
             <p className='description'>{description}</p>          
             <div className='details'>          
               <p className='first-flight'>
-                <span>First Flight:</span> {firstFlight}</p>
+                <span>First Flight:</span> {firstFlight}
+              </p>
               <p className='flight-cost'>
-                <span>Cost Per Flight:</span> {cost}</p>
+                <span>Cost Per Flight:</span> {cost}
+              </p>
             </div>
           </div>
-
           <section className='measurements-wrapper'>
             <div className='measurements'>
               <p>Height:</p> 
@@ -40,7 +39,6 @@ const RocketCards = ({ name, description, cost, firstFlight, image, height, diam
             </div>
           </section>  
         </section>
-
       </section>
     </article>
   )
