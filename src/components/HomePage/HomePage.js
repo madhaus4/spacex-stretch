82185de4245f9cb691c888ad6c310b90394e9469
Maria DelSignore <maria.ad27@gmail.com>
2021-09-16 00:10:11 -0600

    Import rocket img

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,15 +1,15 @@
 import './HomePage.css';
+import rocket from '../../rocket.png'
 
 function HomePage() {
   return (
     <section className='page-title-container'>
       <div className='page-details'>
-        <h1 className='page-header'>An exploration of SpaceX...</h1>
+        <h1 className='page-header tracking-in-contract-bck'>An exploration of SpaceX...</h1>
         <p>The family of Falcon launch vehicles are the first and only orbital class rockets that are resuseable!  They are able to successfully land back on earth AND lower the cost of space access by being able to refly.   </p>
       </div>
-      <div
-        className='animated-rocket-here'
-        >IMG Here
+      <div className='animated-rocket-here'>
+        <img src={rocket} alt='rocket' />
       </div>
     </section>  
   )
