2f3be2b697701407d7ce4f17f1f9535ef6f654ec
StevenTheBearded <stevenberg22@gmail.com>
2021-09-15 20:22:43 -0400

    Add functionality to check incoming data against localstoragr

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -1,8 +1,24 @@
-const cleanData = (data) => {
-  return data.map(element => {
+//need to check if any have the isFavorited === true, if so, return those over the false statement. 
+export const cleanData = (data) => {
+  const updatedData = data.map(element => {
     element.isFavorited = false
     return element
   })
+  checkFavorited(data)
+  return updatedData
 }
 
-export default cleanData;
+export const checkFavorited = (data) => {
+  console.log(localStorage, ' : localstorage');
+ const keys = Object.keys(localStorage)
+ console.log(data, ' :data in utils');
+ console.log(keys, ' :keys in utils');
+  keys.map(key => { 
+    // console.log(key);
+    data.filter(history => {
+      return history.id == localStorage[key] ? history.isFavorited = true : history.isFavorited = false
+    })
+  })
+}
+
+// export default cleanData, checkFavorited;
