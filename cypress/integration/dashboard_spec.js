7f8152c9eda541d8f8fea32ccb091566f4f335b6
Megan McBride <megan.mcbride@colorado.edu>
2021-09-15 21:47:03 -0600

    Add test for article display

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -8,4 +8,21 @@ describe('Dashboard Page View', () => {
   it('Should display a header', () => {
     cy.get('nav').should('be.visible').should('have.class', 'nav-bar')
   })
+
+  it('Should cointain working links to navigate from the dashbaord to the rocket page', () => {
+    cy.get('.rockets-link').click().get('.rocket-card').first().should('be.visible')
+    .get('.history-link').click().get('.articles-wrapper').should('be.visible')      
+  })
+
+  it('Should display a header in the articles section', () => {
+    cy.get('h2').should('contain', 'Recommended For You')
+  })
+
+  it('Should render the available article previews with picture, header, description, and link', () => {
+    cy.get('.articles-wrapper > :nth-child(1)').should('be.visible')
+      .get(':nth-child(1) > .info-wrapper > h3').should('contain', 'First successfull Dragon launch')
+      .get(':nth-child(1) > .info-wrapper > .article-details').should('contain', 'SpaceX becomes the first private company to successfully launch, orbit, and recover a spacecraft.')
+      .get(':nth-child(1) > .info-wrapper > a').should('have.attr', 'href').and('include', 'http://www.bbc.com/news/10209704')
+  })
+
 })
