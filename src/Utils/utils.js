0736c2acc6468c00a28b20c3efa27abd043ead86
StevenTheBearded <stevenberg22@gmail.com>
2021-09-15 21:05:34 -0400

    Create function for functionality

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -1,22 +1,22 @@
 //need to check if any have the isFavorited === true, if so, return those over the false statement. 
 export const cleanData = (data) => {
-  const updatedData = data.map(element => {
+   const updatedData = data.map(element => {
     element.isFavorited = false
     return element
   })
-  checkFavorited(data)
   return updatedData
+  // checkFavorited(updatedData)
 }
 
 export const checkFavorited = (data) => {
-  console.log(localStorage, ' : localstorage');
+  // console.log(localStorage, ' : localstorage');
  const keys = Object.keys(localStorage)
- console.log(data, ' :data in utils');
- console.log(keys, ' :keys in utils');
+//  console.log(data, ' :data in utils');
+//  console.log(keys, ' :keys in utils');
   keys.map(key => { 
-    // console.log(key);
     data.filter(history => {
-      return history.id == localStorage[key] ? history.isFavorited = true : history.isFavorited = false
+      console.log(history, ' :histroy<<<');
+      return history.id == key ? history.isFavorited : !history.isFavorited
     })
   })
 }
