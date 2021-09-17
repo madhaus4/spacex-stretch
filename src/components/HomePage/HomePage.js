7a12bea24a7d1986915134e682d02f660833629e
Megan McBride <megan.mcbride@colorado.edu>
2021-09-16 20:45:37 -0600

    Move image files and attempt to add some smoke to the animation

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,13 +1,16 @@
 import React ,{ useEffect, useRef } from 'react';
 import  { gsap }  from 'gsap';
 import './HomePage.css';
-import rocket from '../../rocket.png'
+import rocketFire from '../../images/rocketFire.png';
+import smoke from '../../images/smoke.png';
 
 function HomePage() {
-  const  rocketRef = useRef(null);
+  const rocketRef = useRef(null);
+  const smokeRef = useRef(null);
   
   useEffect(() => {
-    gsap.to(rocketRef.current,{rotation: -50, y: -1000, x: -800, duration: 10});
+    gsap.to(rocketRef.current,{rotation: -50, y: -1000, x: -800, duration: 10, delay: 0.75});
+    gsap.to(smokeRef.current, {rotation: -50, delay: 1})
   })
 
   return (
@@ -17,7 +20,8 @@ function HomePage() {
         <p>The family of Falcon launch vehicles are the first and only orbital class rockets that are resuseable!  They are able to successfully land back on earth AND lower the cost of space access by being able to refly.   </p>
       </div>
       <div className='animated-rocket-here'>
-        <img src={rocket} alt='rocket' ref={rocketRef}/>
+        <img src={rocketFire} alt='rocket' ref={rocketRef}/>
+        <img src={smoke} alt='smokey-smoke' ref={smokeRef}/>
       </div>
     </section>  
   )
