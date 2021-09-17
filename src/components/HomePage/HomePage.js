e85fd6ce3e551a9b752b1046dc3bd021fe625c94
Megan McBride <megan.mcbride@colorado.edu>
2021-09-16 21:13:41 -0600

    Add styling to the doublepuff clouds

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,16 +1,18 @@
 import React ,{ useEffect, useRef } from 'react';
 import  { gsap }  from 'gsap';
 import './HomePage.css';
-import rocketFire from '../../images/rocketFire.png';
-import smoke from '../../images/smoke.png';
+import rocket from '../../images/rocket.png';
+import croppedcloud from '../../images/croppedcloud.png';
+import twoPuffyStuffs from '../../images/twoPuffyStuffs.png';
 
 function HomePage() {
   const rocketRef = useRef(null);
   const smokeRef = useRef(null);
+  const twoSmokeRef = useRef(null);
   
   useEffect(() => {
     gsap.to(rocketRef.current,{rotation: -50, y: -1000, x: -800, duration: 10, delay: 0.75});
-    gsap.to(smokeRef.current, {rotation: -50, delay: 1})
+    // gsap.to(smokeRef.current, {rotation: -450, delay: 1})
   })
 
   return (
@@ -20,8 +22,9 @@ function HomePage() {
         <p>The family of Falcon launch vehicles are the first and only orbital class rockets that are resuseable!  They are able to successfully land back on earth AND lower the cost of space access by being able to refly.   </p>
       </div>
       <div className='animated-rocket-here'>
-        <img src={rocketFire} alt='rocket' ref={rocketRef}/>
-        <img src={smoke} alt='smokey-smoke' ref={smokeRef}/>
+        <img src={rocket} alt='rocket' ref={rocketRef} className='rocketship'/>
+        <img src={croppedcloud} alt='smokey-smoke' ref={smokeRef} className='puffy-cloud'/>
+        <img src={twoPuffyStuffs} alt='extra-smoke' ref={twoSmokeRef} className='double-puff'/>
       </div>
     </section>  
   )
