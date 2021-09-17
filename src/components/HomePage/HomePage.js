2cd25dc8f8f3a78bf9954766a1edfa284f08cb95
Megan McBride <megan.mcbride@colorado.edu>
2021-09-16 19:48:25 -0600

    Import gsap and test on rocket image

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,7 +1,16 @@
+import React ,{ useEffect, useRef } from 'react';
+import  { gsap }  from 'gsap';
 import './HomePage.css';
 import rocket from '../../rocket.png'
 
 function HomePage() {
+  const rocketRef = useRef();
+  
+  useEffect(() => {
+    console.log(rocketRef)
+    gsap.to(rocketRef.current,{rotation: 0, y: -1000, duration: 10});
+  })
+
   return (
     <section className='page-title-container'>
       <div className='page-details'>
@@ -9,7 +18,7 @@ function HomePage() {
         <p>The family of Falcon launch vehicles are the first and only orbital class rockets that are resuseable!  They are able to successfully land back on earth AND lower the cost of space access by being able to refly.   </p>
       </div>
       <div className='animated-rocket-here'>
-        <img src={rocket} alt='rocket' />
+        <img src={rocket} alt='rocket' ref={rocketRef}/>
       </div>
     </section>  
   )
