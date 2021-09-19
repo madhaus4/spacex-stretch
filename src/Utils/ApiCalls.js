d69730e604583cd8e05b6ef36da8483b650174a3
Megan McBride <megan.mcbride@colorado.edu>
2021-09-19 16:18:39 -0600

    Update offline message

--- src/Utils/ApiCalls.js
+++ src/Utils/ApiCalls.js
@@ -1,5 +1,3 @@
-import App from '../components/App/App'
-
 export const getData = (version, category) => {
   return fetch(`https://api.spacexdata.com/${version}/${category}`)
     .then(res => res.ok ? res.json() : console.log(res))
