c7fbb643927355d7dd91e28da6310b78359ba4e9
Megan McBride <megan.mcbride@colorado.edu>
2021-09-19 15:18:21 -0600

    Adding Elon Musk quotes

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './Error.css';
-import logoLight from '../../TheNXTfrontier-light.svg'
 
 const Error = () => {
 
+const randomizeMusk = () => {
 
+
+}
 
 
   return (
     <div className='error-handling'> 
       <h1 className='error-heading'> Houston, we have a problem...</h1>      
       <p className='error-msg'>Please check your flight path (url) and make any necessary adjustments, Space Cadet. </p>
+      <p>"{randomizeMust}" - Elon Musk</p>
     </div>
   )
 }
