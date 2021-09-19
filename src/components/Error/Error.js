a32bb74a3c6cb1039e49fc5aced7e5f9cadab498
Megan McBride <megan.mcbride@colorado.edu>
2021-09-19 15:44:18 -0600

    Add random Elon quotes

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Error.css';
+import { muskQuotes } from '../../Utils/muskQuotes';
 
 const Error = () => {
 
 const randomizeMusk = () => {
-
+const randomIndex =  Math.floor(Math.random() * muskQuotes.length);
+return muskQuotes[randomIndex]
 
 }
 
@@ -12,8 +14,10 @@ const randomizeMusk = () => {
   return (
     <div className='error-handling'> 
       <h1 className='error-heading'> Houston, we have a problem...</h1>      
-      <p className='error-msg'>Please check your flight path (url) and make any necessary adjustments, Space Cadet. </p>
-      <p>"{randomizeMust}" - Elon Musk</p>
+      <h2 className='error-msg'>Please check your flight path (url) and make any necessary adjustments, Space Cadet. </h2>
+      <div className='musk-wisdom'>
+        <p>"{randomizeMusk()}" - Elon Musk</p>      
+      </div>
     </div>
   )
 }
