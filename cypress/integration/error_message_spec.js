31d8f53bb2a6e0ad415ce10b21859983c153500b
Megan McBride <megan.mcbride@colorado.edu>
2021-09-19 16:52:03 -0600

    Add test for the error catch all page and for the header that displays when there is nothing bookmarked

---
+++ cypress/integration/error_message_spec.js
@@ -0,0 +1,21 @@
+describe('Dashboard Page View', () => {
+  beforeEach(() => {
+    cy.loadArticles('v4', 'history')
+    cy.loadImages('v5', 'launches')
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Should let the user know if there was an error with the url address', () => {
+    cy.visit('http://localhost:3000/hotdog')
+      .get('h1').should('contain', 'Houston, we have a problem')
+      .get('h2').should('contain', 'Please check your flight path')
+  })
+
+  it('Should display a message if there are no bookmarked articles', () => {
+    cy.get('.toggle-view-btn').click().get('.articles-wrapper > h2').should('contain', 'Nothing bookmarked yet')
+  })
+
+  it('Should display an error if the articles do not fetch from the api', () => {
+    
+  })
+})
