9365d4fca7f4b815b5110fdbb8cc57f265534bbc
Megan McBride <megan.mcbride@colorado.edu>
2021-09-19 14:55:01 -0600

    Building out error component

---
+++ src/components/Error/Error.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Error.css';
+
+const Error = () => {
+
+
+  return (
+    <div> 
+      <h1> Houston, we have a problem...</h1>
+      <p>{muskQuote()}</p>
+      <img />
+    </div>
+  )
+}
+
+export default Error;
