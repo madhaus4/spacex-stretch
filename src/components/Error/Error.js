d28c7462dab839fe043ef6f9c7dfa61b76b09d83
Megan McBride <megan.mcbride@colorado.edu>
2021-09-19 15:11:33 -0600

    Add classnames to style the comments

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Error.css';
+import logoLight from '../../TheNXTfrontier-light.svg'
 
 const Error = () => {
 
 
+
+
   return (
-    <div> 
-      <h1> Houston, we have a problem...</h1>
-      <p>{muskQuote()}</p>
-      <img />
+    <div className='error-handling'> 
+      <h1 className='error-heading'> Houston, we have a problem...</h1>      
+      <p className='error-msg'>Please check your flight path (url) and make any necessary adjustments, Space Cadet. </p>
     </div>
   )
 }
