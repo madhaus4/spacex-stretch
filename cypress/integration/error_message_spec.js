e60d138f00c3e490d2fdc4781859fbd542a09578
Megan McBride <megan.mcbride@colorado.edu>
2021-09-19 17:08:59 -0600

    Build test to test for error message on articles not loading

--- cypress/integration/error_message_spec.js
+++ cypress/integration/error_message_spec.js
@@ -1,9 +1,9 @@
 describe('Dashboard Page View', () => {
-  beforeEach(() => {
-    cy.loadArticles('v4', 'history')
-    cy.loadImages('v5', 'launches')
-    cy.visit('http://localhost:3000')
-  })
+  // beforeEach(() => {
+  //   cy.loadArticles('v4', 'history')
+  //   cy.loadImages('v5', 'launches')
+  //   cy.visit('http://localhost:3000')
+  // })
 
   it('Should let the user know if there was an error with the url address', () => {
     cy.visit('http://localhost:3000/hotdog')
@@ -12,10 +12,21 @@ describe('Dashboard Page View', () => {
   })
 
   it('Should display a message if there are no bookmarked articles', () => {
-    cy.get('.toggle-view-btn').click().get('.articles-wrapper > h2').should('contain', 'Nothing bookmarked yet')
+    cy.loadArticles('v4', 'history')
+    cy.loadImages('v5', 'launches')
+    cy.visit('http://localhost:3000')
+      .get('.toggle-view-btn').click().get('.articles-wrapper > h2').should('contain', 'Nothing bookmarked yet')
   })
 
   it('Should display an error if the articles do not fetch from the api', () => {
+    cy.intercept('https://api.spacexdata.com/v4/history', {
+      statusCode: 404
+    })
+    cy.visit('http://localhost:3000')
+      .get('.articles-container > :nth-child(2)').should('contain', 'Loading error')
+  })
+
+  it('Should display an error if the rocket information did not load properly', () => {
     
   })
 })
