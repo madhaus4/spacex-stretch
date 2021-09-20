b9fc0c14e46e94f4aec328f54bfc30400741ee65
StevenTheBearded <stevenberg22@gmail.com>
2021-09-19 20:38:13 -0400

    Add grid display to star container

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,8 +12,11 @@ import logo from '../../TheNXTfrontier.png'
 import star1 from '../../images/star1.png'
 import logoLight from '../../TheNXTfrontier-light.svg'
 import { gsap } from "gsap";
-import { CustomEase } from "gsap/CustomEase";
+import { CustomEase, Strong } from "gsap/CustomEase";
+import { RoughEase } from "gsap/EasePack";
+
 gsap.registerPlugin(CustomEase);
+gsap.registerPlugin(RoughEase);
 
 function App() {
   const [history, setHistory] = useState([])
@@ -22,7 +25,7 @@ function App() {
   const [error, setError] = useState('')
   const twinkleStar = useRef(null)
   
-  let tl1 = gsap.timeline({repeat: 2, repeatDelay: 1 });
+  let tl1 = gsap.timeline();
   // let tl2 = gsap.timeline({repeat: 2, repeatDelay: 2 });
   // let tl3 = gsap.timeline({repeat: 2, repeatDelay: 3});
   // let tl4 = gsap.timeline({repeat: 2, repeatDelay: 4 });
@@ -41,13 +44,16 @@ function App() {
   }
 
   useEffect(() => {
-    tl1.to('.star', { duration: 3, scale:7, ease: "slow(0.7, 2, false)", repeat:-1, yoyo:true})
-    // tl1.to('.star2', { duration: 2, scale:3, ease: "slow(0.7, 2, false)", repeat:-1, yoyo:true})
-    // tl1.to('.star3', { duration: 1, scale:5, ease: "slow(0.7, 2, false)", repeat:-1, yoyo:true})
-    // tl1.to('.star4', { duration: 2, scale:5, ease: "slow(0.7, 2, false)", repeat:-1, yoyo:true})
+    tl1.to('.star1', { duration: 3, scale:4, ease: "steps(12)", repeat:-1, yoyo:true})
+    tl1.to('.star2', { duration: 1, scale:4, ease: "slow(0.7, 2, false)", repeat:-1, yoyo:true})
+    tl1.to('.star3', { duration: 1, scale:2, ease: "elastic.out( 2.5,0.3)", repeat:-1, yoyo:true})
+    tl1.to('.star4', { duration: 1, scale:3, ease: RoughEase.ease.config({strength:3, points:50, taper:"both", randomize:true}), repeat:-1, yoyo:true})
+    tl1.to('.star5', { duration: 3, scale:7, ease: "slow(0.7, 2, false)", repeat:-1, yoyo:true})
     console.log(twinkleStar, ' :twinklestar')
     fetchData();
   }, [])
+
+//   "elastic.out( 2.5,0.3)"
 
   const handleFavorite = (ID) => {
      const found = history.find(story => story.id === ID)
@@ -64,13 +70,17 @@ function App() {
       <img src={star1}  id='twinkle4' ref={twinkleStar} alt='sparkling stars' className='star2 star sparkling-star2' />
       <img src={star1}  id='twinkle5' ref={twinkleStar} alt='sparkling stars' className='star2 star sparkling-star4' />
       <img src={star1}  id='twinkle6' ref={twinkleStar} alt='sparkling stars' className='star2 star sparkling-star3' />
-      <img src={star1}  id='twinkle7' ref={twinkleStar} alt='sparkling stars' className='star2 star sparkling-star2' />
+      <img src={star1}  id='twinkle7' ref={twinkleStar} alt='sparkling stars' className='star2 star sparkling-star5' />
       <img src={star1}  id='twinkle8' ref={twinkleStar} alt='sparkling stars' className='star3 star sparkling-star3' />
       <img src={star1}  id='twinkle9' ref={twinkleStar} alt='sparkling stars' className='star3 star sparkling-star1' />
       <img src={star1}  id='twinkle10' ref={twinkleStar} alt='sparkling stars' className='star3 star sparkling-star3' />
       <img src={star1}  id='twinkle11' ref={twinkleStar} alt='sparkling stars' className='star4 star sparkling-star4' />
       <img src={star1}  id='twinkle12' ref={twinkleStar} alt='sparkling stars' className='star4 star sparkling-star2' />
       <img src={star1}  id='twinkle13' ref={twinkleStar} alt='sparkling stars' className='star4 star sparkling-star4' />
+      <img src={star1}  id='twinkle14' ref={twinkleStar} alt='sparkling stars' className='star5 star sparkling-star5' />
+      <img src={star1}  id='twinkle15' ref={twinkleStar} alt='sparkling stars' className='star5 star sparkling-star4' />
+      <img src={star1}  id='twinkle16' ref={twinkleStar} alt='sparkling stars' className='star5 star sparkling-star3' />
+      <img src={star1}  id='twinkle17' ref={twinkleStar} alt='sparkling stars' className='star5 star sparkling-star2' />
     </div>
     <main>
       <Header />
