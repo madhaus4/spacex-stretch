dce4f082ab4003eb76ac34a94a0ab0072aabe8fe
StevenTheBearded <stevenberg22@gmail.com>
2021-09-20 16:23:16 -0400

    Delete comments

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-// import serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <BrowserRouter>
@@ -13,8 +11,6 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
-// serviceWorker.register();
-// serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
